6a7f524587c26ca2ead7a93cf90d30a712c3a77c
BENIHOUD Youssef <BENIHOUD Youssef@BENIHOUD-LAPTOP>
2017-10-22 13:27:17 +0100

    9adite chi L3ibate f articles/show.php , stikitha chouiya haha + dak Hebergement l'article dialo rah jay mn Base de donneee !

--- public/staff/articles/show.php
+++ public/staff/articles/show.php
@@ -1,13 +1,13 @@
-<?php require_once('../../../private/initialize.php');
+<?php
+require_once('../../../private/initialize.php');
 require_login();
 ?>
 
 <?php
 // $id = isset($_GET['id']) ? $_GET['id'] : '1';
-$id = isset($_GET['id']) ? $_GET['id'] : '1'; 
+$id = isset($_GET['id']) ? $_GET['id'] : '1';
 
 $subject = find_subject_by_id($id);
-
 ?>
 
 <?php $page_title = 'Show Subject'; ?>
@@ -15,43 +15,18 @@ $subject = find_subject_by_id($id);
 
 <div class="section">
     <div class="pageContent">
-        <div id="content">
 
-            <a class="back-link" href="<?php echo url_for('/staff/articles/index.php'); ?>">&laquo; Back to List</a>
-
-            <div class="subject show">
-
-                <h1>Title: <?php echo h($subject['ar_title']); ?></h1>
-
-                <?php 
-                try {
-              
-               /* $tmp = explode('.', du($subject['ar_path']));
-                $ext = end($tmp);
-                
-                //if its docx file
-                if($ext == 'docx'){
-                   $subject['ar_text'] = extracttext(du($subject['ar_path']));
-                }elseif ($ext == 'doc') {
-                    $subject['ar_text'] = readDoc(du($subject['ar_path']));
-                }
-                
-                update_article($subject);
-                */
-                } catch (Exception $ex) {
-                    
-                }
-                
-                ?> 
-                
-                <h3><?php $subject['ar_text'];?></h3>
-
-                <?php update_article($subject)?>
-                
-            </div>
+        <div class="presentation">
+            <h1 class="presentationTitle"><b><?php echo $subject['ar_title']; ?></b></h1>
         </div>
-    </div></div>
+
+        <center>
+            <?php echo $subject['ar_text']; ?>
+        </center>      
 
 
-<?php include(SHARED_PATH . '/Footer.php'); ?>
+    </div>
+</div>
+
+<?php include(SHARED_PATH . './Footer.php'); ?>
 <?php include(SHARED_PATH . '/staff_footer.php'); ?>
